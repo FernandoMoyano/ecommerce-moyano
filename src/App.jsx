0630e684c6abb1a198d4127f2739f082690f411b
FernandoMoyano <fernandomoyano21@gmail.com>
2022-12-15 18:01:41 -0300

    creando context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { CartContext } from "./context/CartContext";
 import "./App";
 import Cart from "./components/CartContainer/CartContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
@@ -8,16 +9,39 @@ import NavBar from "./components/NavBar/NavBar";
 function App() {
 	return (
 		<>
-			<BrowserRouter>
-				<NavBar />
-				<Routes>
-					<Route path='/' element={<ItemListContainer />} />
-					<Route path='/category/:categoryId' element={<ItemListContainer />} />	
-					<Route path='/detail/:productId' element={<ItemDetailContainer />} />	
-					<Route path='/cart' element={<Cart />} />	
-					<Route path='*' element={<Navigate to='/' />} /> 
-				</Routes>
-			</BrowserRouter>
+			<CartContext.Provider value={{}}>
+				<BrowserRouter>
+					<NavBar />
+					<Routes>
+						<Route
+							path="/"
+							element={
+								<ItemListContainer />
+							}
+						/>
+						<Route
+							path="/category/:categoryId"
+							element={
+								<ItemListContainer />
+							}
+						/>
+						<Route
+							path="/detail/:productId"
+							element={
+								<ItemDetailContainer />
+							}
+						/>
+						<Route
+							path="/cart"
+							element={<Cart />}
+						/>
+						<Route
+							path="*"
+							element={<Navigate to="/" />}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</CartContext.Provider>
 		</>
 	);
 }
---
+++ src/context/CartContext.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+export const CartContext=createContext([])
+
+/* export const UseCartContext=useContext(CartContext);
+console.log(CartContext); */
+
+
+
+/* return(
+ <CartContext.Provider>
+
+ </CartContext.Provider>
+) */
